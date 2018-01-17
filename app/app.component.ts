0f9c0369cfe77a06bcdb45602e44a8b3f3245115
Adam Smith and Mikey Pinaud <adam@>
2018-01-17 14:01:46 -0800

    try to add form

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,35 +1,68 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
   <div class='container'>
-    <h1>Hello World</h1>
+    <h1>Kom-Bu-Bu</h1>
+    <div *ngFor="let thisKeg of kegs">
+      <h3>{{thisKeg.name}}</h3>
+      <p>Brewed by {{thisKeg.brewer}} in {{thisKeg.brewerLocation}}</p>
+      <p>Notes of {{thisKeg.flavor}}</p>
+      <p class='caffeine' *ngIf='thisKeg.caffeine'>This Kombucha is caffeinated</p>
+      <p>{{thisKeg.sugarContentInGrams}}g of sugar and {{thisKeg.liveCultures}} live cultures per pint</p>
+      <h5>\${{thisKeg.pintPrice}} per pint</h5>
+      <p>{{thisKeg.pintsRemaining}} pints remaining</p>
+    </div>
+    <div>
+      <form [formGroup]='kegInput' (ngSubmit)='doSubmit($event)'>
+        <input formControlName="name" type="text" placeholder="Keg Name">
+        <input formControlName="brewery" type="text" placeholder="Brewery">
+        <button type="submit">Submit new keg!</button>
+      </form>
+    </div>
   </div>
   `
 })
 
 export class AppComponent {
   kegs: Keg[] = [
-    new Keg('Love', 'Brew Dr.', 'Lavender, Jasmine, Rose', false, 10, 'Portland, OR', 1000000000),
-    new Keg('Trilogy', 'GT\'S', 'Ginger, Lemon, Strawberry', false, 15, 'Beverly Hills, CA', 2000000000),
-    new Keg('Master Brew', 'Kevita', 'Blueberry Basil', true, 14, 'Oxnard, CA', 2000000),
-    new Keg('Amrita Chai', 'Tealixir', 'Ginger, Cardamom, Vanilla Bean, Black Tea', true, 10, 'Nashville, TN', 5000000),
-    new Keg('Simply Nature', 'Ucha Kombucha', 'Ginger', false, 13, 'London, UK', 1000000)
-  ]
+    new Keg('Love', 'Brew Dr.', 'Lavender, Jasmine, Rose', false, 10, 'Portland, OR', 1000000000, 5, 87),
+    new Keg('Trilogy', 'GT\'S', 'Ginger, Lemon, Strawberry', false, 15, 'Beverly Hills, CA', 2000000000, 6, 24),
+    new Keg('Master Brew', 'Kevita', 'Blueberry Basil', true, 14, 'Oxnard, CA', 2000000, 5.5, 124),
+    new Keg('Amrita Chai', 'Tealixir', 'Ginger, Cardamom, Vanilla Bean, Black Tea', true, 10, 'Nashville, TN', 5000000, 5, 9),
+    new Keg('Simply Nature', 'Ucha Kombucha', 'Ginger', false, 13, 'London, UK', 1000000, 7, 11)
+  ];
+
+  // newKeg() {
+  //
+  // }
 }
-
-
 
 export class Keg {
   constructor(
     public name: string,
     public brewer: string,
     public flavor: string,
-    public caffine: boolean,
+    public caffeine: boolean,
     public sugarContentInGrams: number,
     public brewerLocation: string,
     public liveCultures: number,
-
+    public pintPrice: number,
+    public pintsRemaining: number
   ){}
+}
+
+export class NewKegInput {
+  public kegInput = new FormGroup({
+    name: new FormControl('', Validators.required),
+    brewer: new FormControl('', Validators.required)
+  });
+  constructor(public fb: FormBuilder) {}
+  doSubmit(event) {
+    console.log(event);
+    console.log(this.kegInput.value);
+  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
-import { FormsModule }  from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, ReactiveFormsModule, HttpModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
