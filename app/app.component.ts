89fe035cffa9e9ce1f38a342b5f774cb9107c026
Adam Smith and Mikey Pinaud <adam@>
2018-01-17 10:02:08 -0800

    create keg constructor and keg objects

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,5 +10,26 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  
+  kegs: Keg[] = [
+    new Keg('Love', 'Brew Dr.', 'Lavender, Jasmine, Rose', false, 10, 'Portland, OR', 1000000000),
+    new Keg('Trilogy', 'GT\'S', 'Ginger, Lemon, Strawberry', false, 15, 'Beverly Hills, CA', 2000000000),
+    new Keg('Master Brew', 'Kevita', 'Blueberry Basil', true, 14, 'Oxnard, CA', 2000000),
+    new Keg('Amrita Chai', 'Tealixir', 'Ginger, Cardamom, Vanilla Bean, Black Tea', true, 10, 'Nashville, TN', 5000000),
+    new Keg('Simply Nature', 'Ucha Kombucha', 'Ginger', false, 13, 'London, UK', 1000000)
+  ]
+}
+
+
+
+export class Keg {
+  constructor(
+    public name: string,
+    public brewer: string,
+    public flavor: string,
+    public caffine: boolean,
+    public sugarContentInGrams: number,
+    public brewerLocation: string,
+    public liveCultures: number,
+
+  ){}
 }
